294ef64daf6aad063fd9778e350f788fc8df3718
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-05-02 00:43:21 -0400

    working on the icons more

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBriefcaseClock, faPoo } from "@fortawesome/free-solid-svg-icons";
-import { useEffect } from "react";
+import { useEffect, useSyncExternalStore } from "react";
+import { INDEX_SIZE_ERR } from "domexception";
 
 function App() {
   useEffect(() => {
-    const icons = Array.from(document.querySelectorAll("svg"));
+    const icons = document.querySelectorAll("svg");
     const container = document.querySelector(".App");
     const containerWidth = container.offsetWidth;
-    const containerHeight = container.offsetHeight;
+    const containerHeight = container.offsetHeight - 100;
     // alert(containerWidth);
 
     icons.forEach((icon, index) => {
+      console.log(index);
       // Assigning a random starting position and set the style
       const xPos = Math.random() * containerWidth;
       const yPos = Math.random() * containerHeight;
@@ -20,71 +22,53 @@ function App() {
 
       const direction = Math.floor(Math.random() * 4);
 
-      var xSpeed = 1;
-      var ySpeed = 1;
+      var xSpeed = 0;
+      // var ySpeed = 0;
 
-      // console.log(xSpeed);
-      // console.log(ySpeed);
-
-      console.log("old xSpeed: " + xSpeed);
-      console.log("old ySpeed: " + ySpeed);
-
-      // Set a random speed and direction
-      if (direction === 0) {
-        xSpeed = Math.abs(xSpeed);
-        ySpeed = Math.abs(ySpeed);
-      } else if (direction === 1) {
-        icon.style.color = "blue";
-        xSpeed = -Math.abs(xSpeed);
-        ySpeed = Math.abs(ySpeed);
-      } else if (direction === 2) {
-        icon.style.color = "green";
-        xSpeed = -Math.abs(xSpeed);
-        ySpeed = -Math.abs(ySpeed);
-      } else if (direction === 3) {
-        icon.style.color = "yellow";
-        xSpeed = Math.abs(xSpeed);
-        ySpeed = -Math.abs(ySpeed);
+      switch (direction) {
+        case 0:
+          icon.style.color = "red";
+          xSpeed = 1;
+          console.log("red index: " + index);
+          break;
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
       }
-
-      console.log("new xSpeed: " + xSpeed);
-      console.log("new ySpeed: " + ySpeed);
 
       // Move the icon on each animation frame
       function moveIcon() {
         // get current positions
         let currentXPos = parseFloat(icon.style.left);
         let currentYPos = parseFloat(icon.style.top);
-        console.log("currentXPos: " + currentXPos);
-        console.log("currentYPos: " + currentYPos);
+        currentXPos += xSpeed;
+        // currentYPos += ySpeed;
 
         // changing xPos
-        if (currentXPos > containerWidth + 50) {
-          currentXPos = -50;
-        } else if (currentXPos < -50) {
-          currentXPos = containerWidth + 50;
-        } else {
-          currentXPos += xSpeed;
-        }
+        // if (currentXPos > containerWidth + 50) {
+        //   currentXPos = -50;
+        // } else if (currentXPos < -50) {
+        //   currentXPos = containerWidth + 50;
+        // } else {
+        //   currentXPos += xSpeed;
+        // }
 
         // changing yPos
-        if (currentYPos > containerHeight + 50) {
-          currentYPos = -50;
-        } else if (currentYPos < -50) {
-          currentYPos = containerHeight + 50;
-        } else {
-          currentYPos += ySpeed;
-        }
+        // if (currentYPos > containerHeight + 50) {
+        //   currentYPos = -50;
+        // } else if (currentYPos < -50) {
+        //   currentYPos = containerHeight + 50;
+        // } else {
+        //   currentYPos += ySpeed;
+        // }
 
         // update the icon position
         icon.style.left = `${currentXPos}px`;
         icon.style.top = `${currentYPos}px`;
       }
-
-      // moveIcon();
-      // moveIcon();
-      // moveIcon();
-      // moveIcon();
 
       function animate() {
         requestAnimationFrame(animate);
@@ -97,6 +81,11 @@ function App() {
 
   return (
     <div className="App">
+      <FontAwesomeIcon icon={faPoo} />
+      <FontAwesomeIcon icon={faPoo} />
+      <FontAwesomeIcon icon={faPoo} />
+      <FontAwesomeIcon icon={faPoo} />
+      <FontAwesomeIcon icon={faPoo} />
       <FontAwesomeIcon icon={faPoo} />
     </div>
   );
